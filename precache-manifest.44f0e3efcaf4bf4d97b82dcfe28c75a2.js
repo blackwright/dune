c0fdbab69ade05674556b01cdcd52bc71f7b7634
Jerry Gao <gao@blackwright.com>
2020-07-26 21:51:18 -0400

    Updates

---
+++ precache-manifest.44f0e3efcaf4bf4d97b82dcfe28c75a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949df3dadc1d134198dd8afe27af9306",
+    "url": "/dune/index.html"
+  },
+  {
+    "revision": "a76129d4fc18f960d5b7",
+    "url": "/dune/static/css/main.4ca8856b.chunk.css"
+  },
+  {
+    "revision": "c5996a8b1087b97b6d10",
+    "url": "/dune/static/js/2.8a5dd1a3.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/dune/static/js/2.8a5dd1a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76129d4fc18f960d5b7",
+    "url": "/dune/static/js/main.87dfc798.chunk.js"
+  },
+  {
+    "revision": "e87b31b93a327fdf03eb",
+    "url": "/dune/static/js/runtime-main.23484d7e.js"
+  },
+  {
+    "revision": "4dcb4a07bf081ef595e0d5af1342dff3",
+    "url": "/dune/static/media/droid-sans.4dcb4a07.woff2"
+  },
+  {
+    "revision": "6e3eb6e51183792656e90a924b31aa0a",
+    "url": "/dune/static/media/droid-sans.6e3eb6e5.woff"
+  },
+  {
+    "revision": "6f28ad369fa0f2661cc2de7a7be9977e",
+    "url": "/dune/static/media/droid-sans.6f28ad36.ttf"
+  }
+]);
